1d76789f60c02a4e3149f7a6c7a52bd02d994f5e
secretakayungbreezy <146499053+secretakayungbreezy@users.noreply.github.com>
2025-07-17 13:47:41 -0400

    Add files via upload

---
+++ payload.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+clear
+echo "Booting system..."
+sleep 2
+echo "Bypassing firewall..."
+sleep 1
+echo "Cementing backdoor..."
+sleep 1
+echo "Scraping data..."
+sleep 3
+echo "Spoofing MAC address..."
+sleep 5
+echo "Connecting to server..."
+sleep 2
+echo "Booting rootkit..."
+sleep 1
+echo "Uploading payload to mainframe..."
+sleep 1
+echo "This may take a while, feel free to have a snack while the payload uploads."
+cat /dev/zero | pv -v --stop-at-size --size 268400000 -L 1200000
+clear
+sudo echo "Error: Insufficient haxx0r skill"
+echo " "
+echo "Payload exited with Error 3"
+
+
+
